656f777cd8d165579fc285fa164e36c27b4fa4f2
Darya Sharnevich <darya.sharnevich@gmail.com>
2024-10-25 10:31:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## UTP project #1
+
+Connect four (6x7 grid) game application backend in C++
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef UTP_P1_LIBRARY_H
+#define UTP_P1_LIBRARY_H
+
+void hello();
+
+#endif //UTP_P1_LIBRARY_H
